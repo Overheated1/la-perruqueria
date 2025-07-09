c598dab1f757645d518b50087819e12bcc76a58a
overheated <overheated73@gmail.com>
2025-07-09 04:12:56 -0400

    fix: deleting the not found page until the error will be solve

--- app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation"
-import { i18n } from "@/i18n/config"
-
-export default function RootNotFound() {
-  // Redirect to the default locale's 404 page
-  redirect(`/${i18n.defaultLocale}/not-found`)
-}
